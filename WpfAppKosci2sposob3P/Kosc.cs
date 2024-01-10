9bc448266bce9c9d793b46f1a37e48ffa46f334e
markup54 <marzenakupka@tlen.pl>
2024-01-10 15:21:09 +0100

    Dodaj pliki projektów.

---
+++ WpfAppKosci2sposob3P/Kosc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppKosci2sposob3P
+{
+    public class Kosc
+    {
+        public int Wartosc { get; set; }
+        public bool CzyZaznaczona { get; set; }
+    }
+}
---
+++ WpfAppKosci2sposob3P/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppKosci2sposob3P
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<int> rezultaty { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            rezultaty = new ObservableCollection<int>();
+            DataContext = this; // żeby działało bindowanie do XAML
+
+
+        }
+
+        private void rzuc_btn_Click(object sender, RoutedEventArgs e)
+        {
+            rezultaty.Clear();
+            Random random = new Random();
+            for(int i = 0;i<10;i++)
+            {
+                rezultaty.Add(random.Next(1, 7));
+            }
+        }
+
+        private void wyczysc_btn_Click(object sender, RoutedEventArgs e)
+        {
+            rezultaty.Clear();
+            for(int i =0;i<10;i++)
+            {
+                rezultaty.Add(0);
+            }
+
+        }
+    }
+}
